c6eca39707563770ea0f3f7d4a5ed0364108ede3
InkyThunder <98986057+Inkythunder@users.noreply.github.com>
2026-07-18 10:24:10 +0100

    Improved wall rendering

--- Assets/_Project/Scripts/Game/Map/MapRenderer.cs
+++ Assets/_Project/Scripts/Game/Map/MapRenderer.cs
@@ -9,14 +9,13 @@ namespace Roguelike.Game
         [SerializeField] Tilemap floor_tilemap;
         [SerializeField] Tilemap wall_tilemap;
         [SerializeField] TileBase floor_tile;
-        [SerializeField] TileBase wall_face_tile;
-        [SerializeField] TileBase wall_side_left_tile;
-        [SerializeField] TileBase wall_side_right_tile;
+        [SerializeField] TileBase wall_face;
+        [SerializeField] TileBase wall_side_left;
+        [SerializeField] TileBase wall_side_right;
         [SerializeField] TileBase wall_corner_UL;
         [SerializeField] TileBase wall_corner_DL;
         [SerializeField] TileBase wall_corner_DR;
         [SerializeField] TileBase wall_corner_UR;
-        [SerializeField] TileBase wall_top_tile;
 
         public void Render(DungeonMap map)
         {
@@ -72,27 +71,74 @@ namespace Roguelike.Game
         /// <returns></returns>
         TileBase PickWallTile(DungeonMap map, Vector2Int position)
         {
-            if (map[position + Direction.Up] == TileType.Wall &&
-                map[position + Direction.Down] == TileType.Wall &&
-                map[position + Direction.Left] == TileType.Floor &&
-                map[position + Direction.Right] == TileType.Wall) return wall_side_left_tile;
+            TileType up = map[position + Direction.Up];
+            TileType down = map[position + Direction.Down];
+            TileType left = map[position + Direction.Left];
+            TileType right = map[position + Direction.Right];
+            TileType up_right = map[position + Direction.Up_Right];
+            TileType down_right = map[position + Direction.Down_Right];
+            TileType down_left = map[position + Direction.Down_Left];
+            TileType up_left = map[position + Direction.Up_Left];
+
+            if (down == TileType.Floor) return wall_face;
             
-            if (map[position + Direction.Up] == TileType.Wall &&
-                map[position + Direction.Down] == TileType.Wall &&
-                map[position + Direction.Left] == TileType.Wall &&
-                map[position + Direction.Right] == TileType.Floor) return wall_side_right_tile;
+            if (up == TileType.Wall &&
+                down == TileType.Wall &&
+                left == TileType.Floor &&
+                right == TileType.Wall) return wall_side_left;
             
-            if (map[position + Direction.Up] == TileType.Floor &&
-                map[position + Direction.Down] == TileType.Wall &&
-                map[position + Direction.Left] == TileType.Floor &&
-                map[position + Direction.Right] == TileType.Wall) return wall_corner_DR;
+            if (up == TileType.Wall &&
+                down == TileType.Wall &&
+                left == TileType.Wall &&
+                right == TileType.Floor) return wall_side_right;
             
-            if (map[position + Direction.Up] == TileType.Floor &&
-                map[position + Direction.Down] == TileType.Wall &&
-                map[position + Direction.Left] == TileType.Wall &&
-                map[position + Direction.Right] == TileType.Floor) return wall_corner_DL;
+            if (up == TileType.Floor &&
+                down == TileType.Wall &&
+                left == TileType.Floor &&
+                right == TileType.Wall) return wall_corner_DR;
             
-            return wall_face_tile;
+            if (up == TileType.Floor &&
+                down == TileType.Wall &&
+                left == TileType.Wall &&
+                right == TileType.Floor) return wall_corner_DL;
+
+            if (up == TileType.Wall &&
+                down == TileType.Wall &&
+                left == TileType.Wall &&
+                right == TileType.Wall &&
+                up_right == TileType.Floor &&
+                down_right == TileType.Wall &&
+                down_left == TileType.Wall &&
+                up_left == TileType.Wall) return wall_corner_UR;
+            
+            if (up == TileType.Wall &&
+                down == TileType.Wall &&
+                left == TileType.Wall &&
+                right == TileType.Wall &&
+                up_right == TileType.Wall &&
+                down_right == TileType.Wall &&
+                down_left == TileType.Wall &&
+                up_left == TileType.Floor) return wall_corner_UL;
+            
+            if (up == TileType.Wall &&
+                down == TileType.Wall &&
+                left == TileType.Wall &&
+                right == TileType.Wall &&
+                up_right == TileType.Wall &&
+                down_right == TileType.Floor &&
+                down_left == TileType.Wall &&
+                up_left == TileType.Wall) return wall_side_right;
+            
+            if (up == TileType.Wall &&
+                down == TileType.Wall &&
+                left == TileType.Wall &&
+                right == TileType.Wall &&
+                up_right == TileType.Wall &&
+                down_right == TileType.Wall &&
+                down_left == TileType.Floor &&
+                up_left == TileType.Wall) return wall_side_left;
+            
+            return wall_face;
         }
         
         public void Clear()
